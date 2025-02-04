89dd30102c5fac4cd142bcd2ce9337dec523d854
vdbthomas <vandenbergthomas@hotmail.fr>
2025-02-04 15:01:04 +0100

    système de connexion fonctionne

---
+++ HandiBlog/src/app/Services/auth.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {User} from '../Models/User';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private apiUrl = 'http://localhost:8080/api/users/login';
+  constructor( private http: HttpClient) {
+
+  }
+login(email: string, password: string) {
+  return this.http.post<User>(this.apiUrl, {email, password})
+
+  }
+}
