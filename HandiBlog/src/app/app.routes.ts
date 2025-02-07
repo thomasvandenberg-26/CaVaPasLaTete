46740b12ba668460783b44aaa97faeb1bb9e0a5c
thomasvandenberg-26 <vandenbergthomas@hotmail.fr>
2025-02-07 16:20:40 +0100

    recuperation de l'api pour le login

--- HandiBlog/src/app/app.routes.ts
+++ HandiBlog/src/app/app.routes.ts
@@ -1,8 +1,6 @@
 import { Routes } from '@angular/router';
 import { PageInscriptionComponent} from './Views/page-inscription/page-inscription.component';
 
-
-
 export const routes: Routes = [
-  {path :'signup', component: PageInscriptionComponent},
+  {path :'inscription', component: PageInscriptionComponent}
 ];
