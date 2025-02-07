ae76aa3c5f1dd94487a71426a552bdb938f4b7db
vdbthomas <vandenbergthomas@hotmail.fr>
2025-02-07 16:20:44 +0100

    supressions de choses inutiles dans app.component et ajout de couleurs dans tailwind config js ajout route profile

--- HandiBlog/src/app/app.component.ts
+++ HandiBlog/src/app/app.component.ts
@@ -5,7 +5,7 @@ import {HeaderComponent} from './Views/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
