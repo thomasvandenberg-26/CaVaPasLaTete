62d59ea2ff3b722205ad0d3d7d38833fddcad4ad
vdbthomas <vandenbergthomas@hotmail.fr>
2025-02-07 15:23:45 +0100

    configs

---
+++ src/main/java/org/example/handiblog_back/HandiBlogBackApplication.java
@@ -0,0 +1,13 @@
+package org.example.handiblog_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandiBlogBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HandiBlogBackApplication.class, args);
+    }
+
+}
