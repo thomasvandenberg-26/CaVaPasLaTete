4262c3db66eab798acf3a41d1fbfc3838afed9e9
vdbthomas <vandenbergthomas@hotmail.fr>
2025-02-07 16:20:41 +0100

    finalisation du style de la page inscription

--- HandiBlog/tailwind.config.js
+++ HandiBlog/tailwind.config.js
@@ -4,7 +4,36 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+
+      fontFamily: {
+        primary: 'Roboto',
+        nunito: 'Nunito'
+      },
+      backgroundColor: {
+        yellow_sun: '#DAB84A',
+        blancbleu: '#E2F0EF',
+        header: '#A9D4D6',
+        green : '#42A390',
+        testblanc: '#F6F5F1',
+      },
+      textColor : {
+        light_gray: '#90A6B4',
+        green : '#42A390',
+        rouge: '#C46960',
+        bleubleubleu : '#82C3C5'
+
+      },
+      borderColor :{
+        blue_light : '#0277A2',
+        bleu : '#82C3C5',
+        bleucanard: '#065758',
+        vertvertvert: '#42A390'
+
+      },
+
+
+    },
   },
   plugins: [],
 }
