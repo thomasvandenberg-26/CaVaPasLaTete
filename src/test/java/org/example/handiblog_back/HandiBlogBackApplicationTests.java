ef1e5caf1c478fe44d5eb270869d4020ab9d242e
vdbthomas <vandenbergthomas@hotmail.fr>
2025-02-07 15:23:45 +0100

    configs

---
+++ src/test/java/org/example/handiblog_back/HandiBlogBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.handiblog_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandiBlogBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
