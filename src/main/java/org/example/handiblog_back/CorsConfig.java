33e27f7cb134c3852396fc93f30d6122662773fe
vdbthomas <vandenbergthomas@hotmail.fr>
2025-02-07 15:23:45 +0100

    configs

---
+++ src/main/java/org/example/handiblog_back/CorsConfig.java
@@ -0,0 +1,27 @@
+package org.example.handiblog_back;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Autorise toutes les routes.
+                        .allowedOrigins("http://localhost:4200/signup") // Autorise uniquement cette origine
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Méthodes HTTP autorisées
+                        .allowedHeaders("Content-Type", "Authorization", "X-Requested-With", "Accept")
+                        .allowCredentials(true);
+
+                         // Autorise tous les headers
+
+            }
+        };
+    }
+}
