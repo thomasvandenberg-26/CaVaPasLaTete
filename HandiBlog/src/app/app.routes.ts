7bfdd69eaa8e3c36b7a1d4432426aa29eecb1252
vdbthomas <vandenbergthomas@hotmail.fr>
2025-02-07 16:28:47 +0100

    suppression route profile et app header dans app component

--- HandiBlog/src/app/app.routes.ts
+++ HandiBlog/src/app/app.routes.ts
@@ -1,10 +1,9 @@
 import { Routes } from '@angular/router';
 import { PageInscriptionComponent} from './Views/page-inscription/page-inscription.component';
-import {PageProfilComponent} from './Views/page-profil/page-profil.component';
+
 
 
 
 export const routes: Routes = [
   {path :'signup', component: PageInscriptionComponent},
-  {path :'profile', component: PageProfilComponent},
 ];
