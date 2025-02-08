7ef49429ff8c069cd80ab169565fd6238511eed3
vdbthomas <vandenbergthomas@hotmail.fr>
2025-02-08 10:47:30 +0100

    je rebase develop sur le depot distant

--- HandiBlog/src/app/app.component.ts
+++ HandiBlog/src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from './Views/header/header.component';
 
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, ],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
