a1296424d0923e2d1379fa5c886380b3a4b73f55
vdbthomas <vandenbergthomas@hotmail.fr>
2025-02-08 10:51:05 +0100

    couleur

--- HandiBlog/tailwind.config.js
+++ HandiBlog/tailwind.config.js
@@ -14,35 +14,32 @@ module.exports = {
         yellow_sun: '#DAB84A',
         blancbleu: '#E2F0EF',
         header: '#A9D4D6',
-        green: '#42A390',
-        bleucanard: '#065758',
-        bleubleu: '#82C3C5',
+        green : '#42A390',
+
+        bleubleu : '#82C3C5',
         testblanc: '#F6F5F1',
       },
-      textColor: {
+      textColor : {
         light_gray: '#90A6B4',
-        green: '#42A390',
+        green : '#42A390',
         rouge: '#C46960',
-        bleubleubleu: '#82C3C5',
-        bleucanard: '#065758',
-        blancbleu: '#E2F0EF',
-
-
+        bleubleubleu : '#82C3C5',
+        bleucanard : '#065758'
 
       },
-      borderColor: {
-        blue_light: '#0277A2',
-        bleu: '#82C3C5',
+      borderColor :{
+        blue_light : '#0277A2',
+        bleu : '#82C3C5',
         bleucanard: '#065758',
         vertvertvert: '#42A390'
 
       },
       width: {
-        "60p": "60%",
-        "40p": "40%"
+        "60p" : "60%",
+        "40p" : "40%",
+      }
 
-      },
     },
-    plugins: [],
-  }
+  },
+  plugins: [],
 }
