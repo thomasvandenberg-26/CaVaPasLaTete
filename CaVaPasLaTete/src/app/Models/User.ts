97911def19ded89dd8f28563abacf1530dc6e3b2
vdbthomas <vandenbergthomas@hotmail.fr>
2025-04-07 22:57:35 +0200

    suppresion de parametres utilisateur inutile dans model user

--- CaVaPasLaTete/src/app/Models/User.ts
+++ CaVaPasLaTete/src/app/Models/User.ts
@@ -1,11 +1,14 @@
+
+import {Observable} from 'rxjs';
+
 export class User{
  constructor(
-   public id: number,
    public email: string,
    public password: string,
-   public firstName: string,
-   public lastName: string,
-
  ) {
+
+
  }
+
+
 }
