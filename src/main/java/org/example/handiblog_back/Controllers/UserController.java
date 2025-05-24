02bac7ade2edf7e1171a43f7224353e8b84f557b
vdbthomas <vandenbergthomas@hotmail.fr>
2025-05-24 12:12:29 +0200

    correction de la connexion : une personne se connecte que si elle connait son mot de passe. (userController : login) un utilisateur recupere son id après la connexion pour afficher les bonnes infos par la suite

--- src/main/java/org/example/handiblog_back/Controllers/UserController.java
+++ src/main/java/org/example/handiblog_back/Controllers/UserController.java
@@ -5,11 +5,11 @@ import org.example.handiblog_back.Models.User;
 import org.example.handiblog_back.Repository.UserRepository;
 import org.example.handiblog_back.Services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 
 @RestController
 @RequestMapping("/api/users")
@@ -19,8 +19,10 @@ public class UserController {
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
     @Autowired
     private UserService userService  ;
+    @Autowired
+    private UserRepository userRepository;
 
-     @PostMapping("/create")
+    @PostMapping("/create")
     public ResponseEntity<User> registerUser(@RequestBody User user)
     {
         logger.info("Données reçues : {}", user.getNom() + " " + user.getPrenom() + " " + user.getEmail() + " " + user.getType());
@@ -28,9 +30,16 @@ public class UserController {
        return ResponseEntity.ok(savedUser);
     }
     @PostMapping("/login")
-    public boolean login(@RequestBody User user)
-    {
-        return userService.authenticate(user.getEmail(), user.getPassword());
+    public ResponseEntity<?> login(@RequestBody User user)
+    { logger.info("backend test login " + user.getEmail());
+        User connectedUser = userService.authenticate(user.getEmail(), user.getPassword());
+        if(connectedUser != null)
+        {
+            return ResponseEntity.ok(connectedUser);
+        }
+        else{
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Email ou mot de passe incorrect");
+        }
     }
     @PostMapping("/update")
     public User updateUserSpecialite(@RequestBody User user)
@@ -41,5 +50,16 @@ public class UserController {
     public int getUserId(String email)
     {
         return userService.getUserId(email);
+    }
+
+    @GetMapping("{id}/getFirstName")
+    public String getUserFirstName(@PathVariable int id )
+    {
+        return userRepository.findUserFirstNameById(id);
+    }
+    @GetMapping("{id}/getLastName")
+    public String getUserLastName(@PathVariable int id )
+    {
+        return userRepository.findUserLastNameById(id);
     }
 }
